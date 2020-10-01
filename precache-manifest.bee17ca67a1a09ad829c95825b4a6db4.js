8dffb939c611265892cfa0a3fb84a8ec9f4fe023
krishnaraj8991 <krishnarajs078@gmail.com>
2020-10-01 20:48:04 +0530

    Updates

---
+++ precache-manifest.bee17ca67a1a09ad829c95825b4a6db4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a0966fcf2d356e11e55903ce6cbeef",
+    "url": "/AlgoVis/index.html"
+  },
+  {
+    "revision": "1b88e0adf058926f2e2a",
+    "url": "/AlgoVis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b466d5f9305bf83cdf2",
+    "url": "/AlgoVis/static/js/2.3abc7d80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AlgoVis/static/js/2.3abc7d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d150505a1a7c2a899628",
+    "url": "/AlgoVis/static/js/3.76a509c6.chunk.js"
+  },
+  {
+    "revision": "1b88e0adf058926f2e2a",
+    "url": "/AlgoVis/static/js/main.f2c922d1.chunk.js"
+  },
+  {
+    "revision": "e4c962b7486e16f063bc",
+    "url": "/AlgoVis/static/js/runtime-main.c4723263.js"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/AlgoVis/static/media/caret-down.ca82bc74.svg"
+  },
+  {
+    "revision": "8c7a23c1ff27d51937b69f3fc6851b87",
+    "url": "/AlgoVis/static/media/crescent-moon-svgrepo-com.8c7a23c1.svg"
+  },
+  {
+    "revision": "448b9283da81dce183152e5b32047d88",
+    "url": "/AlgoVis/static/media/innerring.448b9283.svg"
+  },
+  {
+    "revision": "05246fb60750c39c9bca082efdb67a3e",
+    "url": "/AlgoVis/static/media/loadingIcon5.05246fb6.svg"
+  },
+  {
+    "revision": "21dfcf4c73ddc7a92957f1e99a34a83d",
+    "url": "/AlgoVis/static/media/middlering.21dfcf4c.svg"
+  },
+  {
+    "revision": "4bc350235b9b211252226b1dda141853",
+    "url": "/AlgoVis/static/media/outerring.4bc35023.svg"
+  },
+  {
+    "revision": "baa40420dc4753556b9a7a10a5f4a682",
+    "url": "/AlgoVis/static/media/pauseIcon.baa40420.svg"
+  },
+  {
+    "revision": "56e1dc7c5f6013b3be8403ff29bea906",
+    "url": "/AlgoVis/static/media/pauseIcon2.56e1dc7c.svg"
+  },
+  {
+    "revision": "0dd89474e340e57d742d62e0a4b4bbbb",
+    "url": "/AlgoVis/static/media/playIcon.0dd89474.svg"
+  },
+  {
+    "revision": "9e63145336e766fc0899150e48ab61d9",
+    "url": "/AlgoVis/static/media/playIcon2.9e631453.svg"
+  },
+  {
+    "revision": "b8724df75cd72d88f35a09b66faaa1e1",
+    "url": "/AlgoVis/static/media/portalActive.b8724df7.svg"
+  },
+  {
+    "revision": "4fa7be80cbeb03ba739fbe9579283129",
+    "url": "/AlgoVis/static/media/start.4fa7be80.svg"
+  },
+  {
+    "revision": "eefbcdcb6e9f593967a50a4482eb6bf3",
+    "url": "/AlgoVis/static/media/sun-svgrepo-com.eefbcdcb.svg"
+  },
+  {
+    "revision": "696a6c6dde2309445aaddc32f13faa8e",
+    "url": "/AlgoVis/static/media/target.696a6c6d.svg"
+  }
+]);
